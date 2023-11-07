dc91f50660fb02d8cef852c4c61290b4fe6d084b
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-11-07 14:15:45 +0100

    Add project files.

---
+++ SolarPanelsCompany/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SolarPanelsCompany
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
