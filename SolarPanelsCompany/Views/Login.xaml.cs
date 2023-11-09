7fada68bacb60d6730387c1fa83f85898f692b84
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-11-09 10:05:59 +0100

    .

--- SolarPanelsCompany/Views/Login.xaml.cs
+++ SolarPanelsCompany/Views/Login.xaml.cs
@@ -2,9 +2,27 @@ namespace SolarPanelsCompany.Views;
 
 public partial class Login : ContentPage
 {
+
+    public const String username = "aleleotta";
+    public const String password = "fakePassword2023";
+
     public Login()
     {
         InitializeComponent();
     }
 
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        if (usernameField.Text == username && passwordField.Text == password)
+        {
+            usernameField.Text = "";
+            passwordField.Text = "";
+            await Navigation.PushModalAsync(new Home(), true);
+        }
+        else {
+            usernameField.Text = "";
+            passwordField.Text = "";
+            await DisplayAlert("Error", "The username or password is incorrect.", "Close");
+        }
+    }
 }
