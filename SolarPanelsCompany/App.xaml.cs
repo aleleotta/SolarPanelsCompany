487938bc540fc58582f38d6b83d56420ca719d0f
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-11-12 00:03:23 +0100

    .

--- SolarPanelsCompany/App.xaml.cs
+++ SolarPanelsCompany/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace SolarPanelsCompany
+﻿using SolarPanelsCompany.Views;
+
+namespace SolarPanelsCompany
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new Home();
         }
     }
 }
